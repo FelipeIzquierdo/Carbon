1eb5978730a192a0deb319e3dd37821075cb6dc9
LUIS FELIPE IZQUIERDO <felipeizquierdoo@gmail.com>
2016-04-19 16:25:21 -0500

    Update es.php
    ampliando traducción de español

--- src/Carbon/Lang/es.php
+++ src/Carbon/Lang/es.php
@@ -1,19 +1,18 @@
 <?php
-
-/*
- * This file is part of the Carbon package.
- *
- * (c) Brian Nesbitt <brian@nesbot.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-/**
- * Translation messages.  See http://symfony.com/doc/current/book/translation.html
- * for possible formats.
- */
-return array(
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+    'ago'       => 'hace :time',
+    'from_now'  => 'dentro de :time',
+    'after'     => ':time después',
+    'before'    => ':time antes',
     'year'      => '1 año|:count años',
     'month'     => '1 mes|:count meses',
     'week'      => '1 semana|:count semanas',
@@ -21,8 +20,23 @@ return array(
     'hour'      => '1 hora|:count horas',
     'minute'    => '1 minuto|:count minutos',
     'second'    => '1 segundo|:count segundos',
-    'ago'       => 'hace :time',
-    'from_now'  => 'dentro de :time',
-    'after'     => ':time después',
-    'before'    => ':time antes',
-);
+    'january'   => 'enero',
+    'february'  => 'febrero',
+    'march'     => 'marzo',
+    'april'     => 'abril',
+    'may'       => 'mayo',
+    'june'      => 'junio',
+    'july'      => 'julio',
+    'august'    => 'agosto',
+    'september' => 'septiembre',
+    'october'   => 'octubre',
+    'november'  => 'noviembre',
+    'december'  => 'diciembre',
+    'monday'    => 'lunes',
+    'tuesday'   => 'martes',
+    'wednesday' => 'miércoles',
+    'thursday'  => 'jueves',
+    'friday'    => 'viernes',
+    'saturday'  => 'sábado',
+    'sunday'    => 'domingo',
+];
